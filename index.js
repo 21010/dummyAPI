bab0e840fde766d63da141d9b5699e41576e1db7
Ziolo <Grzegorz.Ziolo@pl.nsg.com>
2020-02-23 23:10:57 +0100

    Update: configure cors access

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = express();
 
 server.use(cors({ allowMethods: ['GET', 'PUT', 'POST', 'DELETE'] }));
 server.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "no-cors");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
